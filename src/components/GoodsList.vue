1aaadcb31df06bea1d94d101068829a75521b788
Ipatov Vitalii <ipatov_va@taximaxim.ru>
2020-12-07 14:10:10 +0500

    Tasks from Unit 3.4

---
+++ src/components/GoodsList.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul class="catalog__list">
+    <goods-item v-for="(item, index) in goods" :item="item" :key="index" />
+  </ul>  
+</template>
+
+
+<script>
+import GoodsItem from './GoodsItem.vue'
+export default {
+  components: { GoodsItem },
+  props: ['goods']
+}
+</script>
---
+++ src/data/goods.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    title: 'ADAM A8X',
+    price: '82920',
+    imgID: '20201207_0001'
+  },
+  {
+    title: 'Arturia Keylab 88 MKII',
+    price: '99500',
+    imgID: '20201207_0002'
+  },
+  {
+    title: 'ESI U168 XT',
+    price: '42330',
+    imgID: '20201207_0003'
+  },
+  {
+    title: 'Focusrite scarlett 18i20 3rd gen',
+    price: '49299',
+    imgID: '20201207_0004'
+  },
+  {
+    title: 'KORG C1',
+    price: '81000',
+    imgID: '20201207_0005'
+  },
+  {
+    title: 'KORG KRONOS2 88',
+    price: '331000',
+    imgID: '20201207_0006'
+  },
+  {
+    title: 'KURZWEIL M1',
+    price: '78100',
+    imgID: '20201207_0007'
+  },
+  {
+    title: 'KURZWEIL PC3 LE7',
+    price: '125300',
+    imgID: '20201207_0008'
+  },
+  {
+    title: 'M-AUDIO BX8 D3',
+    price: '20490',
+    imgID: '20201207_0009'
+  },
+  {
+    title: 'MOTU Ultralite MKIII Hybrid',
+    price: '60180',
+    imgID: '20201207_0010'
+  },
+  {
+    title: 'Native Instruments Komplete Kontrol S88 MKII',
+    price: '112000',
+    imgID: '20201207_0011'
+  },
+  {
+    title: 'Novation Launchkey 61 MK3',
+    price: '27500',
+    imgID: '20201207_0012'
+  },
+  {
+    title: 'YAMAHA CLP 735R',
+    price: '149990',
+    imgID: '20201207_0013'
+  },
+  {
+    title: 'YAMAHA HS8',
+    price: '26900',
+    imgID: '20201207_0014'
+  },
+  {
+    title: 'YAMAHA MX88 bk',
+    price: '99990',
+    imgID: '20201207_0015'
+  },
+]
