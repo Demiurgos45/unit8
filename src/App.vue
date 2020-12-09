d3532d24956e4ccc71494cd5075cb688bbe90785
Ipatov Vitalii <ipatov_va@taximaxim.ru>
2020-12-09 14:20:09 +0500

    Tasks from Unit 4.5

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,80 @@
 <template>
-  <section class="catalog">
-    <goods-list :goods="goods" />
-    <base-pagination v-model="currentPage" :elements-count="goodsCount" :elements-per-page="goodsPerPage" />
-  </section>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        Количество наименований: {{ goodsCount }}
+      </span>
+    </div>
+
+    <div class="content__catalog">
+      <goods-filter 
+        :price-from.sync="filterPriceFrom"
+        :price-to.sync="filterPriceTo"
+        :category-id.sync="filterCategoryId"
+        :current-page.sync="currentPage"
+      />
+
+      <section class="catalog">
+        <goods-list :goods="goodsList" />
+        <base-pagination
+          v-model="currentPage"
+          :elements-count="goodsCount"
+          :elements-per-page="goodsPerPage"
+        />
+      </section>
+      
+    </div>
+  </main>
 </template>
 
 <script>
 import BasePagination from './components/BasePagination.vue'
+import GoodsFilter from './components/GoodsFilter.vue'
 import GoodsList from './components/GoodsList'
 import goods from './data/goods'
 
 export default {
   name: 'App',
-  components: {GoodsList, BasePagination},
+  components: {GoodsList, BasePagination, GoodsFilter},
   data() {
     return {
       currentPage: 1,
       goodsPerPage: 3,
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
     }
   },
 
   computed: {
-    goods() {
+    filteredGoods() {
+      let filteredGoods = goods
+
+      if (this.filterCategoryId > 0) {
+        filteredGoods = filteredGoods.filter(item => item.categoryId === this.filterCategoryId)
+      }
+      
+      if (this.filterPriceFrom > 0) {
+        filteredGoods = filteredGoods.filter(item => item.price > this.filterPriceFrom)
+      }
+
+      if (this.filterPriceTo > 0) {
+        filteredGoods = filteredGoods.filter(item => item.price < this.filterPriceTo)
+      }
+
+      return filteredGoods
+    },
+
+    goodsList() {
       const listOffset = (this.currentPage - 1) * this.goodsPerPage
-      return goods.slice(listOffset, listOffset + this.goodsPerPage)
+      return this.filteredGoods.slice(listOffset, listOffset + this.goodsPerPage)
     },
 
     goodsCount() {
-      return goods.length
+      return this.filteredGoods.length
     }
   }
 }
